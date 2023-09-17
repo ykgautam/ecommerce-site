56d982f34097c8deb83c9e43505f7c37ff48670e
Yash <yk.gautam97@gmail.com>
2023-09-17 19:30:30 +0530

    JWT token and spring securities

---
+++ src/main/java/com/yash/config/JwtConstant.java
@@ -0,0 +1,9 @@
+package com.yash.config;
+
+public class JwtConstant {
+
+    public static final String SECRET_KEY="jhdkhkhdsshdgvkjsvakjajlckjljchjk";
+
+    public static final String JWT_HEADER="Authorization";
+
+}
